05e0e570b44c4b766d4e4f3aa1f59a11fd19ae17
bhatia <friendsat8>
2017-12-28 16:26:10 -0500

    impl logging. Also impl filters must be in json properties file; add desc element top describe the filter

--- scripts/run_prosper_loans_filter example.sh
+++ scripts/run_prosper_loans_filter example.sh
@@ -1,2 +1,2 @@
 cd ../bin
-./prosper_loans_filter.exe "../src/prosper_marketplace/creds/creds.json"
+./prosper_loans_filter.exe "../src/prosper_marketplace/creds/creds.json" "../src/prosper_marketplace/filters/filter_typeC1.json"
--- src/prosper_marketplace/prosper_loans_filter.go
+++ src/prosper_marketplace/prosper_loans_filter.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-  "fmt"
   "time"
+  "io"
   "io/ioutil"
   "os"
+  "log"
   "encoding/json"
   "github.com/bhatia4/gofn-prosper/prosper"
   "github.com/bhatia4/gofn-prosper/prosper/auth"
@@ -18,55 +19,168 @@ type ProsperCreds struct {
     Password		string `json:"password"`
 }
 
-func main() {
-	raw, err := ioutil.ReadFile(os.Args[1])
+type Filter struct {
+    ShortDesc		string `json:"shortDesc"`
+    LongDesc		string `json:"LongDesc"`
+	DtiWprosperLoan	interval.Float64Range `json:"dtiWprosperLoan"`
+	EstimatedReturn	interval.Float64Range `json:"estimatedReturn"`
+	IncomeRange		[]int8 `json:"incomeRange"`
+	Limit			int8   `json:limit`
+	DaysFromCurrentTime int8 `json:daysFromCurrentTime`
+	ListingStatus 	[]int8 `json:listingStatus`
+	ListingTerm		[]int8 `json:listingTerm`
+	Offset			int8   `json:offset`
+	Rating			[]string `json:rating`
+}
+
+type MyFloat64Range interval.Float64Range
+func (o *MyFloat64Range) UnmarshalJSON(data []byte) error {
+	Trace.Printf("%s\n", data)
+	var v [2]float64
+	if err := json.Unmarshal(data, &v); err != nil {
+		Error.Println(err.Error())
+		return err
+	}
+	Trace.Println("%+v\n", v)
+	o.Min = &v[0]
+	o.Max = &v[1]
+	return nil
+}
+
+var (
+    Trace   *log.Logger
+    Info    *log.Logger
+    Warning *log.Logger
+    Error   *log.Logger
+)
+
+func Init(
+    traceHandle io.Writer,
+    infoHandle io.Writer,
+    warningHandle io.Writer,
+    errorHandle io.Writer) {
+
+    Trace = log.New(traceHandle,
+        "TRACE: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Info = log.New(infoHandle,
+        "INFO: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Warning = log.New(warningHandle,
+        "WARNING: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+
+    Error = log.New(errorHandle,
+        "ERROR: ",
+        log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+
+func readFromFile(filename string) []byte {
+	raw, err := ioutil.ReadFile(filename)
     if err != nil {
-        fmt.Println(err.Error())
+        Error.Println(err.Error())
         os.Exit(1)
     }
+	return raw
+}
 
+func main() {
+	//setup logging
+	Init(ioutil.Discard /*when testing, replace w/ os.Stdout*/, 
+			os.Stdout, os.Stdout, os.Stderr)
+	
+	//read creds json file
 	var creds ProsperCreds
-    json.Unmarshal(raw, &creds)
+    json.Unmarshal(readFromFile(os.Args[1]), &creds)
 
+	//read filter json file
+	var currFilter Filter
+    json.Unmarshal(readFromFile(os.Args[2]), &currFilter)
+
+	Info.Printf("%+v\n", currFilter)
+	
 	client := prosper.NewClient(auth.ClientCredentials{
   		ClientID:     creds.ClientID,
   		ClientSecret: creds.ClientSecret,
   		Username:     creds.Username,
   		Password:     creds.Password,
 	})
-
 	account, err := client.Account(prosper.AccountParams{})
 	if err != nil {
-  		fmt.Printf("Failed to retrieve account information: %v", err)
+  		Error.Printf("Failed to retrieve account information: %v", err)
   		return
 	}
-	fmt.Printf("Your account has $%.2f in cash and a total value of $%.2f\n",
+	
+	Trace.Printf("Your account has $%.2f in cash and a total value of $%.2f\n",
 	  	account.AvailableCashBalance, account.TotalAccountValue)
 
+
+	var inputRatings []prosper.Rating = make([]prosper.Rating, len(currFilter.Rating));
+	for index := range currFilter.Rating {
+		curr, err := prosper.ParseRating(currFilter.Rating[index])
+		if err != nil {
+			Error.Println(err.Error())
+			os.Exit(1)
+		}
+		inputRatings[index] = curr
+	}
+	
+	var inputListingStatus []prosper.ListingStatus = make([]prosper.ListingStatus, len(currFilter.ListingStatus));
+	for index := range currFilter.ListingStatus {
+		curr, err := prosper.ParseListingStatus(int64(currFilter.ListingStatus[index]))
+		if err != nil {
+			Error.Println(err.Error())
+			os.Exit(1)
+		}
+		inputListingStatus[index] = curr
+	}
+	
+	var inputIncomeRange []prosper.IncomeRange = make([]prosper.IncomeRange, len(currFilter.IncomeRange));
+	for index := range currFilter.IncomeRange {
+		curr, err := prosper.ParseIncomeRange(int64(currFilter.IncomeRange[index]))
+		if err != nil {
+			Error.Println(err.Error())
+			os.Exit(1)
+		}
+		inputIncomeRange[index] = curr
+	}
+	
+	var inputListingTerm []prosper.ListingTerm = make([]prosper.ListingTerm, len(currFilter.ListingTerm));
+	for index := range currFilter.ListingTerm {
+		curr, err := prosper.ParseListingTerm(int64(currFilter.ListingTerm[index]))
+		if err != nil {
+			Error.Println(err.Error())
+			os.Exit(1)
+		}
+		inputListingTerm[index] = curr
+	}
+	
 	searchResp, err := client.Search(prosper.SearchParams{
   		Offset: 0,
  		Limit:  20,
   		Filter: prosper.SearchFilter{
-    		Rating:      		[]prosper.Rating{prosper.RatingC},
-			ListingStartDate: 	interval.NewTimeRange(time.Now().AddDate(0, 0, -5), time.Now()),//going back atmost 2 days
-			ListingStatus: 		[]prosper.ListingStatus{prosper.ListingActive},
-			DtiWprosperLoan: 	interval.NewFloat64Range(0.0, 0.225),
-			EstimatedReturn: 	interval.NewFloat64Range(0.0851, 0.2),
-			IncomeRange:		[]prosper.IncomeRange{prosper.Between75kAnd100k, prosper.Over100k},
-			ListingTerm:		[]prosper.ListingTerm{prosper.ListingTerm60Months},
+    		Rating:      		inputRatings,
+			ListingStartDate: 	interval.NewTimeRange(time.Now().AddDate(0, 0, int(-1*currFilter.DaysFromCurrentTime)), time.Now()),
+			ListingStatus: 		inputListingStatus,
+			DtiWprosperLoan: 	currFilter.DtiWprosperLoan,
+			EstimatedReturn: 	currFilter.EstimatedReturn,
+			IncomeRange:		inputIncomeRange,
+			ListingTerm:		inputListingTerm,
   		},
 	})
 	if err != nil {
-  		fmt.Printf("Failed to search available note listings: %v\n", err)
+  		Error.Printf("Failed to search available note listings: %v\n", err)
   		return
 	}
 
-	fmt.Printf("Found %d matching notes, processing first %d\n",
+	Info.Printf("Found %d matching notes, processing first %d\n",
   		searchResp.TotalCount, searchResp.ResultCount)
-//	fmt.Printf("Second filter - remove loans based on income range and listing term\n")
 
 	for i, listing := range searchResp.Results {
-		fmt.Printf("%2d: ID:%v; Rating:%s; Status:%s; Amount:$%5.0f; Listed on:%s; Delinquencies last 7yrs:%d; Est Return:%.2f%%; Term:%d; Income Range:%d (%s); Last 6 mos. Inquiries:%d; Debt-to-Income Ratio:%.2f%%; Prior Prosper Loans(Late Payments 1 mo+:%d; Bal. Outstanding:%.2f)\n",
+		Info.Printf("%2d: ID:%v; Rating:%s; Status:%s; Amount:$%5.0f; Listed on:%s; Delinquencies last 7yrs:%d; Est Return:%.2f%%; Term:%d; Income Range:%d (%s); Last 6 mos. Inquiries:%d; Debt-to-Income Ratio:%.2f%%; Prior Prosper Loans(Late Payments 1 mo+:%d; Bal. Outstanding:%.2f)\n",
 				i+1, 
 				listing.ListingNumber, 
 				prosper.RatingToString(listing.Rating),
