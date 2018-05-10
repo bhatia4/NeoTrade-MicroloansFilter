07981f997d7ebedbc8e2060cb4307245719f12bd
bhatia <friendsat8>
2018-05-09 21:28:12 -0400

    new filters w/ higher minimums;

---
+++ scripts/prosper_loans_filter_typeC1.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+##add this to crontab to autorun:
+## 30 2 * * 1-5  cd /root/NeoTrade-MicroloansFilter/scripts && sh prosper_loans_filter_typeC.sh 2>> /tmp/neotrade_prosper_loans_filter.err 1>> /tmp/neotrade_prosper_loans_filter.log
+
+cd ../bin
+./prosper_loans_filter "../src/prosper_marketplace/creds/creds.json" "../src/prosper_marketplace/filters/filter_typeC1_8.501min.json" "+12483284008"
