da53f97827cfcb811f8656f32d0b6d520d245b0f
bhatia <friendsat8>
2019-07-03 22:58:55 -0400

    modify scripts, filters and code to all desc prefix strings thus allowing us to change SMS text on the fly in case carrier blocks it again

--- scripts/prosper_loans_filter_typeA1.sh
+++ scripts/prosper_loans_filter_typeA1.sh
@@ -3,4 +3,4 @@
 ## 30 2 * * 1-5  cd /root/NeoTrade-MicroloansFilter/scripts && sh prosper_loans_filter_typeA.sh 2>> /tmp/neotrade_prosper_loans_filter.err 1>> /tmp/neotrade_prosper_loans_filter.log
 
 cd ../bin
-./prosper_loans_filter "../src/prosper_marketplace/creds/creds.json" "../src/prosper_marketplace/filters/filter_typeA1_5.3min.json" "+12483284008"
+./prosper_loans_filter "../src/prosper_marketplace/creds/creds.json" "../src/prosper_marketplace/filters/filter_typeA1_5.3min.json" "+12482472871"
